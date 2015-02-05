753470b4c709aaf1733f7b6e69ea44d35eec7a9a
sowbiba <isow@profideo.com>
2015-02-05 16:42:37 +0100

    Fix namespace in composer

--- src/Profideo/FormulaInterpretorBundle/FormulaInterpretorBundle.php
+++ src/Profideo/FormulaInterpretorBundle/FormulaInterpretorBundle.php
@@ -2,6 +2,7 @@
 namespace Profideo\FormulaInterpretorBundle;
 
 use Profideo\FormulaInterpretorBundle\Command\FormulainterpretorChangeAttributeVisibilityCommand;
+use Symfony\Component\Console\Application;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class FormulaInterpretorBundle extends Bundle
