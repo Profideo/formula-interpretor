38effd57d55a806db8303791dfcbad73128fe656
Florent SEVESTRE <aniki.taicho@gmail.com>
2015-04-22 16:30:15 +0200

    Removes registerCommands in FormulaInterpretorBundle

--- Profideo/FormulaInterpretorBundle/FormulaInterpretorBundle.php
+++ Profideo/FormulaInterpretorBundle/FormulaInterpretorBundle.php
@@ -1,18 +1,9 @@
 <?php
+
 namespace Profideo\FormulaInterpretorBundle;
 
-use Profideo\FormulaInterpretorBundle\Command\FormulainterpretorChangeAttributeVisibilityCommand;
-use Symfony\Component\Console\Application;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class FormulaInterpretorBundle extends Bundle
 {
-    /**
-     * {@inheritDoc}
-     */
-    public function registerCommands(Application $application)
-    {
-        // Register command
-        $application->add(new FormulainterpretorChangeAttributeVisibilityCommand());
-    }
 }
