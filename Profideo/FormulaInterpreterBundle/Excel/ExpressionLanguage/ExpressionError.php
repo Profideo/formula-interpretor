232c44811577db003fbc8f5670e5c8a05fe2eff1
bmehdi <ebrahimi@profideo.com>
2017-03-20 09:58:48 +0100

    Improve wording

--- Profideo/FormulaInterpreterBundle/Excel/ExpressionLanguage/ExpressionError.php
+++ Profideo/FormulaInterpreterBundle/Excel/ExpressionLanguage/ExpressionError.php
@@ -6,6 +6,6 @@ class ExpressionError extends \LogicException
 {
     public function __construct($message)
     {
-        parent::__construct(sprintf('%s.', $message));
+        parent::__construct(sprintf('%s', $message));
     }
 }
